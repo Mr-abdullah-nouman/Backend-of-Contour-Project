b0ce420a0c0037888231cc0766c5f5db36e37a22
Abdullah Nouman <121357377+abdnmn1997@users.noreply.github.com>
2023-03-14 21:14:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const connecttodb = require("./config/db");
+const bodyParser = require("body-parser");
+const router = require("./router/router");
+const cors = require("cors");
+var whitelist = ['http://localhost:3000']
+
+
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }
+  } else {
+    corsOptions = { origin: false }
+  }
+  callback(null, corsOptions) 
+}
+const app = express();
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors(corsOptionsDelegate))
+app.use("/user/", router);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  connecttodb();
+  console.log(`app is listining on port ${PORT}`);
+});
